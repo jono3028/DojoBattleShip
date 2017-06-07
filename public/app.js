3250f175519925ffd982cdb14001cfedfad28f4d
jono3028 <jonowen@hotmail.com>
2017-06-07 16:50:17 -0700

    basic lobby functionality

--- public/app.js
+++ public/app.js
@@ -13,6 +13,9 @@ jQuery(function($){
     bindEvents: () => {
 
       IO.socket.on('connected', IO.onConnected)
+      IO.socket.on('PlayerEntersLobby', Battle.Player.addPlayer)
+      IO.socket.on('Receive Challenge Request', Battle.Player.receiveChallengeRequest)
+      IO.socket.on('Goto Pregame', Battle.Player.setShips)
       IO.socket.on('Game Room Message', Battle.roomMessage)
       IO.socket.on('playersTurn', Battle.playersTurn)
       IO.socket.on('playMade', Battle.playMade)
@@ -30,7 +33,7 @@ jQuery(function($){
     shot: [],
 
     init: () => {
-      Battle.showLobby() 
+      Battle.showLogin() 
       Battle.cachePage()
       Battle.bindEvents() 
     },
@@ -39,19 +42,26 @@ jQuery(function($){
       Battle.$doc = $(document)
       // Templates
       Battle.$gameArea = $('#gameArea')
+      Battle.$templateLogin = $('#login-template').html()
       Battle.$templateLobby = $('#lobby-template').html()
+      Battle.$templateChallenge = $('#challenge-template').html()
       Battle.$templateSetGrid = $('#setGrid-template').html()
       Battle.$templateGamePlay = $('#gamePlay-template').html()
     },
 
     bindEvents: () => {
-      Battle.$doc.on('click', '#btnSetShips', Battle.Player.setShipsClick)
+      Battle.$doc.on('click', '#btnToLobby', Battle.Player.enterLobby)
       Battle.$doc.on('click', '#btnSaveShips', Battle.Player.saveShipsClick)
       Battle.$doc.on('click', '#btnFireAtWill', Battle.gameFireClick)
       Battle.$doc.on('click', '#btnReset', function () {IO.socket.emit('gameReset')})
     },
-    showLobby: () => {
-      Battle.$gameArea.html(Battle.$templateLobby)
+    showLogin: () => {
+      IO.socket.emit('Join Room', 'Global')
+      
+      Battle.$gameArea.html(Battle.$templateLogin)
+    },
+    initChallenge: (event) => {
+     
     },
     makeGameRoom: (playerGrid) => {
       Battle.$gameArea.html(Battle.$templateGamePlay)
@@ -78,6 +88,7 @@ jQuery(function($){
       $('.firingBoard td').hover(function () {$(this).css('background','red')}, function () {$(this).css('background','')})
       IO.socket.emit('playerEntersRoom', data)
     },
+    
     roomMessage: (data) => {
       $('#gamePlay-message').html(data.message)
     },
@@ -106,10 +117,43 @@ jQuery(function($){
       playerName: '',
       shipCoord: [[0,0],[0,1],[0,2],[0,3],[0,4]], // Ship position [data-x, data-y]
 
-      setShipsClick: () => {
-        console.log('Player - setShipsClick')
+      enterLobby: () => {
         Battle.Player.playerName = $('#playerName').val() || 'Captian Jack'
-        Battle.numPlayersInRoom++
+        Battle.SocketID = IO.socket.id
+        // IO.socket.emit('connected', {roomName: Battle.SocketID})
+        Battle.$gameArea.html(Battle.$templateLobby)
+        // $('#playerList').append('li').html(Battle.Player.playerName)
+        data = {id: Battle.SocketID, name: Battle.Player.playerName}
+        IO.socket.emit('New Player', data)
+        console.log(`${Battle.Player.playerName} entering lobby`)
+      },
+      addPlayer: function (data) {
+        $('#playerList').append(`<li><button class="btnChallengePlayer" data="${data.id}">${data.name}</button></li>`)
+        $('.btnChallengePlayer').click(function () {
+          var newdata = { 
+            challengedPlayerId: $(this).attr('data'),
+            challengerPlayerId: Battle.SocketID,
+            challengerPlayerName: Battle.Player.playerName
+          }
+          console.log(newdata)
+          IO.socket.emit('Send Challenge Request', newdata)
+          IO.socket.emit('Change Room', {old: 'Global', new: 'Room1'})
+          
+        })
+        console.log('addPlayer')
+      },
+      receiveChallengeRequest: function (data) {
+        Battle.$gameArea.html(Battle.$templateChallenge)
+        console.log('receiveChallenge')
+        $('#btnYes').click(function () {
+          IO.socket.emit('Accept Challenge')
+          IO.socket.emit('Change Room', {old: 'Global', new: 'Room1'})
+        })
+      },
+      setShips: () => {
+        console.log('Player - setShipsClick')
+        // Battle.Player.playerName = $('#playerName').val() || 'Captian Jack'
+        // Battle.numPlayersInRoom++
         Battle.$gameArea.html(Battle.$templateSetGrid)
         console.log(Battle.Player.playerName)
       },
